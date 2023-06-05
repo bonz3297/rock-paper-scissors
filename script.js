aae0203c35632ad19935c708e6165dc844a3cb6e
bonz3297 <bonz3297@gmail.com>
2023-06-05 16:26:42 +0300

    Finish basic UI for the game.

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ function getComputerChoice() {
 	return arr[Math.floor(Math.random() * 3)];
 }
 
+const scoreboard = document.querySelector('#scoreboard');
+scoreboard.textContent = `Player: 0, Computer: 0, Draw: 0`;
+
+const buttons = document.querySelectorAll('.rps-button');
+buttons.forEach((button) =>
+	button.addEventListener('click', (e) => {
+		let round = playRound(e.target.textContent.toLowerCase(), getComputerChoice());
+		updateScoreboard(round);
+	})
+);
+
 function playRound(playerSelection, computerSelection) {
-	const computer = computerSelection();
-	const player = playerSelection.toLowerCase();
+	const computer = computerSelection;
+	const player = playerSelection;
 	console.log(`player: ${player}, computer: ${computer}`);
 	if (player === computer) return 'Draw!';
 
@@ -28,22 +39,33 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game(roundsToWin) {
-	let playerWins = 0;
-	let computerWins = 0;
-	let draws = 0;
+	let round = '';
 
-	while (true) {
-		let round = playRound(prompt('Rock or Paper or Scissors?'), getComputerChoice);
-
-		if (round.includes('win')) playerWins++;
-		else if (round.includes('lose')) computerWins++;
-		else if (round.includes('Draw')) draws++;
-
-		console.log(`Player: ${playerWins}, Computer: ${computerWins}, Draws: ${draws}`);
-
-		if (playerWins === roundsToWin) return alert('Player won the game!');
-		if (computerWins === roundsToWin) return alert('Computer won the game!');
-	}
+	if (playerWins === roundsToWin) return alert('Player won the game!');
+	if (computerWins === roundsToWin) return alert('Computer won the game!');
 }
 
-game(2);
+// const gameBtn = document.querySelector('#game-btn');
+// gameBtn.addEventListener('click', (e) => {
+// 	game(5);
+// });
+
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0;
+
+function updateScoreboard(round) {
+	if (round.includes('win')) playerWins++;
+	else if (round.includes('lose')) computerWins++;
+	else if (round.includes('Draw')) draws++;
+
+	scoreboard.textContent = `Player: ${playerWins}, Computer: ${computerWins}, Draws: ${draws}`;
+}
+
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', (e) => {
+	playerWins = 0;
+	computerWins = 0;
+	draws = 0;
+	scoreboard.textContent = `Player: ${playerWins}, Computer: ${computerWins}, Draws: ${draws}`;
+});
